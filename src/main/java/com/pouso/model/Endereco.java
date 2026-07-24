03c4bb5126c569f06653870f217ce3cf4dc68842
Adriele Colossi <adriele.colossi4@gmail.com>
2026-07-24 14:19:25 -0300

    feat: add home html/css; add models para adocao, avaliacao, devolucao, endereco, notificacao and status

---
+++ src/main/java/com/pouso/model/Endereco.java
@@ -0,0 +1,93 @@
+package com.pouso.model;
+
+public class Endereco {
+
+    private String usuario_cpf;
+    private String cep;
+    private String rua;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+
+    public Endereco() {
+    }
+
+    public Endereco(String usuario_cpf, String cep, String rua,
+                    String numero, String complemento,
+                    String bairro, String cidade, String uf) {
+        this.usuario_cpf = usuario_cpf;
+        this.cep = cep;
+        this.rua = rua;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+    }
+
+    public String getUsuario_cpf() {
+        return usuario_cpf;
+    }
+
+    public void setUsuario_cpf(String usuario_cpf) {
+        this.usuario_cpf = usuario_cpf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+}
